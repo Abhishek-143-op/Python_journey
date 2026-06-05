c73e214fad3cebd750cee2f397b6eb59c7bb143c
Abhishek <ab8188036@gmail.com>
2026-06-05 23:42:27 +0530

    creating very simple project on loops

---
+++ 05_Loops/simpro5.py
@@ -0,0 +1,28 @@
+# ----------IN this Exercise we are doin loops practice-------------
+
+""" print using loop 
+*
+**
+***
+****
+*****
+"""
+
+n=int(input("Enter the no. of rows : "))
+
+for i in range(0,n+1):
+    for j in range(1,i):
+        print("*", end=" ")
+    print()
+
+
+"""print
+*****
+****
+***
+**
+*
+"""
+for i in range(0,n+1):
+    for j in range():
+        pass
