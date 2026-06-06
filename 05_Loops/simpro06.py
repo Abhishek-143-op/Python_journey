166dec0864fde27ad4a9941b0828c2f10cbea313
Abhishek <ab8188036@gmail.com>
2026-06-06 23:05:34 +0530

    Learning widely famous Fabonacci series

---
+++ 05_Loops/simpro06.py
@@ -0,0 +1,27 @@
+# fabinocaii series 
+
+
+
+# with this project i decided to use practically in the webpages Favinocaii series 
+# widely famous series .abs
+
+n=int(input("Enter the no. : "))
+a=0
+b=1
+print(a , b, end=" ")
+
+for i in range (n):
+
+    """
+    # a , b= b , a+b  # without using 3rd variable . 
+    This is VVVVIP 
+
+    SWAPPING WIHOUT USING 3RD VARIABLE 
+    
+    """
+    
+    c=a+b
+    a=b
+    b=c
+    
+    print(c, end=" ")
